622a34fd738579ce54bc6af1eb85588ec6b9c595
Patricia <pilar.zq03@gmail.com>
2021-06-05 13:52:22 -0500

    Create index and app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+//Ejercicio 01
+function sumar(a,b){
+    console.log(a+b);
+}
+//sumar(4,60);
+
+//Ejercicio 02
+function promedio(n1,n2,n3,n4){
+    console.log((n1+n2+n3+n4)/4)
+}
+//promedio(20,20,20,15);
+
+//Ejercicio 03
+function rectangulo(base,alt){
+    console.log((base*alt));
+}
+//rectangulo(5,6);
+
+//Ejercicio 04
+function triangulo(b,h){
+    console.log((b*h)/2);
+}
+//triangulo(20,30);
+
+//Ejercicio 05
+function circunferencia(r){
+    const pi=3.1416;
+    console.log(`El area de la circunferencia es: ${pi*(r*r)}`);
+}
+//circunferencia(4);
+
+//Ejercicio 06 ``
+function pagoSemanal(pagoDia,h){
+    const pago = (pagoDia*h)*7;
+    console.log(`Su pago semanal es: ${pago}`); 
+}
+//pagoSemanal(35,8);
+
+//Ejercicio 07
+function pulgadas(m){
+    const plg = 0.0254 ;
+    console.log(`${m} metros son : ${m/plg} pulgadas`);
+}
+//pulgadas(25);
+
+//Ejercicio 08
+function dolares(s){
+    const dl = 3.85;
+    console.log(`s/.${s} soles son : ${s/dl} dolares`);
+}
+//dolares(35);
+
+//Ejercicio 09
+function anioNac(e){
+    const anioActual = 2021;
+    console.log(`Su edad actual es: ${anioActual-e}`);
+}
+//anioNac(1999);
+
+//Ejercicio 10
+//function persona(n1,n2,n3,e1,e2,e3){
+    const nom = ['cristina','mario','xiomara'];
+    const edad = [25,10,83];
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Preguntas
+***
+# 1. ¿Cuál fue la razón de crear Javascript?
+***
+se buscaba un lenguaje que se ejecutara en el mismo navegador,para asi optimizar el tiempo del usuario al navegar.tambien para volver msa dinamicas las paginas, para asi volverlas mas atarctivas ala vista del usuario.
+
+***
+
+# 2.¿Quién fue el creador de Javascript?
+***
+El creador de Javascript es Brendan Eich , quien fue contratado por Netscape para crear un lenguaje que funcionara en su navegador.
+***
+# 3.¿Por qué se llamó Javascript? algoritmo expuesto en este caso es el único con el que cuenta TuTienda?
+***
+en un principio Eich lo nombro LiveScript pero, ya que ,la palabra Java estaba de moda en el mundo informatico se renombro como JavaScript justo antes de su lanzamiento.
